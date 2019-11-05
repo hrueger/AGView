af01b5666fb2611d38bd131662e993459578d161
Hannes Rüger <hannesrueger@gmx.de>
2019-11-04 20:27:39 -1000

    added encoding support for windows

--- src/app/_components/dashboard/dashboard.component.ts
+++ src/app/_components/dashboard/dashboard.component.ts
@@ -1,6 +1,11 @@
-import { Component, ElementRef, ViewChild } from "@angular/core";
+import { Component, ElementRef, NgZone, ViewChild } from "@angular/core";
 import { SplitComponent } from "angular-split";
+import * as avconv from "avconv";
+import { remote } from "electron";
+import * as fs from "fs";
 import { FileSystemFileEntry, NgxFileDropEntry } from "ngx-file-drop";
+import * as path from "path";
+import * as smalltalk from "smalltalk";
 import * as VideoContext from "videocontext";
 import { Slide } from "../../_entities/slide";
 
@@ -25,6 +30,8 @@ export class DashboardComponent {
   private videoCtx: VideoContext;
   private currentNode: any;
 
+  constructor(private zone: NgZone) { }
+
   public ngAfterViewInit() {
 
     this.previewCanvas = this.previewCanvasRef.nativeElement;
@@ -41,18 +48,73 @@ export class DashboardComponent {
     this.videoCtx.play();
   }
 
-  public dropped(files: NgxFileDropEntry[]) {
-    for (const droppedFile of files) {
-      if (droppedFile.fileEntry.isFile) {
-        const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
+  public fileEntryToFile(fileEntry: FileSystemFileEntry) {
+      return new Promise<File>((resolve, reject) => {
         fileEntry.file((file: File) => {
-          this.slides.push(new Slide("video", file.path, "CROSSFADE", 1));
-        });
-      }
-    }
+              resolve(file);
+          });
+      });
   }
 
-  public setCurrentSlide(event, slideIdx: number) {
+  public async dropped(files: NgxFileDropEntry[]) {
+    const encodedFiles: string[] = [];
+    const unencodedFiles: string[] = [];
+
+    for (const droppedFile of files) {
+      if (droppedFile.fileEntry.isFile) {
+        const file = await this.fileEntryToFile(droppedFile.fileEntry as FileSystemFileEntry);
+        const filename = file.path.replace(/\.[^/.]+$/, "");
+        const extension = file.path.split(".").pop();
+        if (filename.endsWith("encoded")) {
+          encodedFiles.push(`${filename}.${extension}`);
+        } else if (fs.existsSync(`${filename}.encoded.${extension}`)) {
+          encodedFiles.push(`${filename}.encoded.${extension}`);
+        } else {
+          unencodedFiles.push(`${filename}.${extension}`);
+        }
+      }
+    }
+    if (unencodedFiles.length > 0) {
+      smalltalk.confirm("Unencoded files found", "Some files are not encoded in the best format to give the best\
+      performance.\
+      Press 'Ok' to encode them now.<br>If you already encoded them , please make sure that the files are called\
+      'path/to/file/filename.encoded.ext'!")
+      .then(() => {
+        for (let index = 0; index < unencodedFiles.length; index++) {
+          const file = unencodedFiles[index];
+          const filename = file.replace(/\.[^/.]+$/, "");
+          const extension = file.split(".").pop();
+          const fileoutput = `${filename}.encoded.${extension}`;
+          const params = [
+            "-i", file,
+            "-tune", "fastdecode",
+            "-strict", "experimental",
+            fileoutput,
+          ];
+          const os = "windows";
+          const binfile = "avconv.exe";
+          const stream = avconv(params, path.join(remote.app.getAppPath(), "bin", "avconv", os, binfile));
+          const progress =
+            smalltalk.progress("Encoding files", `Encoding file ${index + 1} of ${unencodedFiles.length}`);
+          stream.on("message", (data: any) => {
+              // console.log("message: ", data);
+          });
+          stream.on("progress", (p: any) => {
+            this.zone.run(() => {
+              progress.setProgress(Math.round(p * 100)).catch(() => {
+                stream.kill();
+              });
+            });
+          });
+          // end when finished
+        }
+      })
+      .catch(() => undefined);
+    }
+    // this.slides.push(new Slide("video", file.path, "CROSSFADE", 1));
+  }
+
+  public setCurrentSlide(event: any, slideIdx: number) {
 
     this.currentSlide = this.slides[slideIdx];
 
@@ -83,14 +145,14 @@ export class DashboardComponent {
     // console.log("Canvas sized to", this.previewCanvas.width, this.previewCanvas.height);
   }
 
-  public mouseOverVideo(event, slideIdx) {
+  public mouseOverVideo(event, slideIdx: string | number) {
     const rect = event.target.getClientRects()[0];
     const x = ( event.clientX - rect.x ) / rect.width;
     event.target.currentTime = x * event.target.duration;
     this.slides[slideIdx].percent = x * 100;
   }
 
-  public mouseLeave(event, slideIdx) {
+  public mouseLeave(event: { target: { querySelector: (arg0: string) => void; }; }, slideIdx: string | number) {
     this.slides[slideIdx].showProgressbar = false;
     const video = event.target.querySelector("video");
     this.seekToThumbnail(slideIdx, video);
