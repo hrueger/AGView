ed80b1869b5d0a0f9ce17b65f885e939796a1c82
Hannes Rüger <hannesrueger@gmx.de>
2020-05-15 16:56:00 +0200

    fixed more linting errors

--- src/worker/obs.ts
+++ src/worker/obs.ts
@@ -1,14 +1,14 @@
+/* eslint-disable no-console */
 import * as osn from "obs-studio-node";
 import { BrowserWindow } from "electron";
 import { Subject } from "rxjs";
-import { first } from "rxjs/operators"
 import * as path from "path";
 import * as fs from "fs";
 import * as uuid from "uuid/v4";
 import { IInput, IScene } from "obs-studio-node";
 
 export class OBS {
-    private obsInitialized: boolean = false;
+    private obsInitialized = false;
     private signals: Subject<any> = new Subject();
     public previewWindow: BrowserWindow;
     private sources: IInput[] = [];
@@ -36,7 +36,9 @@ export class OBS {
         setInterval(() => {
             try {
                 win.webContents.send("performanceStatistics", osn.NodeObs.OBS_API_getPerformanceStatistics());
-            } catch { }
+            } catch {
+                //
+            }
         }, 1000);
     }
 
@@ -46,14 +48,13 @@ export class OBS {
         osn.NodeObs.SetWorkingDirectory(path.join(__dirname, "../../node_modules/obs-studio-node"));
 
         const obsDataPath = path.join(__dirname, "../../osn-data"); // OBS Studio configs and logs
-        // Arguments: locale, path to directory where configuration and logs will be stored, your application version
         const initResult = osn.NodeObs.OBS_API_initAPI("en-US", obsDataPath, "1.0.0");
 
         if (initResult !== 0) {
             const errorReasons = {
                 "-2": "DirectX could not be found on your system. Please install the latest version of DirectX for your machine here <https://www.microsoft.com/en-us/download/details.aspx?id=35?> and try again.",
                 "-5": "Failed to initialize OBS. Your video drivers may be out of date, or Streamlabs OBS may not be supported on your system.",
-            }
+            };
 
             const errorMessage = errorReasons[initResult.toString()] || `An unknown error #${initResult} was encountered while initializing OBS.`;
 
@@ -88,6 +89,7 @@ export class OBS {
         const videoSource = osn.InputFactory.create("monitor_capture", "desktop-video");
 
         // Get information about prinary display
+        // eslint-disable-next-line
         const { screen } = require("electron");
         const primaryDisplay = screen.getPrimaryDisplay();
         const realDisplayWidth = primaryDisplay.size.width * primaryDisplay.scaleFactor;
@@ -95,9 +97,9 @@ export class OBS {
         const aspectRatio = realDisplayWidth / realDisplayHeight;
 
         // Update source settings:
-        let settings = videoSource.settings;
-        settings["width"] = realDisplayWidth;
-        settings["height"] = realDisplayHeight;
+        const { settings } = videoSource;
+        settings.width = realDisplayWidth;
+        settings.height = realDisplayHeight;
         videoSource.update(settings);
         videoSource.save();
 
@@ -113,7 +115,6 @@ export class OBS {
         const sceneItem = this.scenes[0].add(videoSource);
         sceneItem.scale = { x: 1.0 / this.videoScaleFactor, y: 1.0 / this.videoScaleFactor };
 
-        // Tell recorder to use this source (I'm not sure if this is the correct way to use the first argument `channel`)
         osn.Global.setOutputSource(1, this.scenes[0]);
     }
 
@@ -142,6 +143,7 @@ export class OBS {
         this.previewWindow.on("resize", () => {
             const [width, height] = this.previewWindow.getSize();
             osn.NodeObs.OBS_content_resizeDisplay(displayId, width, height);
+            osn.NodeObs.OBS_content_setPaddingSize(displayId, 0);
         });
 
         osn.NodeObs.OBS_content_createSourcePreviewDisplay(
@@ -158,62 +160,71 @@ export class OBS {
         this.previewWindow.close();
     }
 
-    public addFile(path: string) {
-        console.log("add", path)
-        const realpath = fs.realpathSync(path);
+    public addFile(p: string) {
+        console.log("add", p);
+        const realpath = fs.realpathSync(p);
         const SUPPORTED_EXT = {
-            image_source: ['png', 'jpg', 'jpeg', 'tga', 'bmp'],
-            ffmpeg_source: [
-                'mp4',
-                'ts',
-                'mov',
-                'flv',
-                'mkv',
-                'avi',
-                'mp3',
-                'ogg',
-                'aac',
-                'wav',
-                'gif',
-                'webm',
+            imageSource: ["png", "jpg", "jpeg", "tga", "bmp"],
+            ffmpegSource: [
+                "mp4",
+                "ts",
+                "mov",
+                "flv",
+                "mkv",
+                "avi",
+                "mp3",
+                "ogg",
+                "aac",
+                "wav",
+                "gif",
+                "webm",
             ],
-            browser_source: ['html'],
-            text_gdiplus: ['txt'],
+            browserSource: ["html"],
+            textGDIplus: ["txt"],
         };
-        let ext = realpath.split('.').splice(-1)[0];
+        let ext = realpath.split(".").splice(-1)[0];
         if (!ext) return null;
         ext = ext.toLowerCase();
-        const filename = path.split('\\').splice(-1)[0];
+        const filename = p.split("\\").splice(-1)[0];
 
         const types = Object.keys(SUPPORTED_EXT);
         for (const type of types) {
+            // eslint-disable-next-line no-continue
             if (!SUPPORTED_EXT[type].includes(ext)) continue;
             let settings = null;
-            if (type === 'image_source') {
+            if (type === "imageSource") {
                 settings = { file: path };
-            } else if (type === 'browser_source') {
+            } else if (type === "browserSource") {
                 settings = {
+                    // eslint-disable-next-line @typescript-eslint/camelcase
                     is_local_file: true,
+                    // eslint-disable-next-line @typescript-eslint/camelcase
                     local_file: path,
                 };
-            } else if (type === 'ffmpeg_source') {
+            } else if (type === "ffmpegSource") {
                 settings = {
+                    // eslint-disable-next-line @typescript-eslint/camelcase
                     is_local_file: true,
+                    // eslint-disable-next-line @typescript-eslint/camelcase
                     local_file: path,
                     looping: true,
                 };
-            } else if (type === 'text_gdiplus') {
+            } else if (type === "textGDIplus") {
                 settings = {
+                    // eslint-disable-next-line @typescript-eslint/camelcase
                     read_from_file: true,
                     file: path,
                 };
             }
             if (settings) {
-                const s = this.createSource(filename, type, settings)
+                const s = this.createSource(filename, type, settings);
                 const sceneItem = this.scenes[0].add(s);
-                sceneItem.scale = { x: 1.0 / this.videoScaleFactor, y: 1.0 / this.videoScaleFactor };
+                sceneItem.scale = {
+                    x: 1.0 / this.videoScaleFactor,
+                    y: 1.0 / this.videoScaleFactor,
+                };
                 return s;
-            };
+            }
         }
         return null;
     }
@@ -243,7 +254,7 @@ export class OBS {
             osn.NodeObs.IPC.disconnect();
             this.obsInitialized = false;
         } catch (e) {
-            throw Error("Exception when shutting down OBS process" + e);
+            throw Error(`Exception when shutting down OBS process${e}`);
         }
 
         console.debug("OBS shutdown successfully");
@@ -260,8 +271,8 @@ export class OBS {
         // Getting settings container
         const settings = osn.NodeObs.OBS_settings_getSettings(category).data;
 
-        settings.forEach(subCategory => {
-            subCategory.parameters.forEach(param => {
+        settings.forEach((subCategory) => {
+            subCategory.parameters.forEach((param) => {
                 if (param.name === parameter) {
                     oldValue = param.currentValue;
                     param.currentValue = value;
@@ -282,19 +293,23 @@ export class OBS {
             return [];
         }
 
-        const subcategorySettings = categorySettings.find(sub => sub.nameSubCategory === subcategory);
+        const subcategorySettings = categorySettings.find(
+            (sub) => sub.nameSubCategory === subcategory,
+        );
         if (!subcategorySettings) {
             console.warn(`There is no subcategory ${subcategory} for OBS settings category ${category}`);
             return [];
         }
 
-        const parameterSettings = subcategorySettings.parameters.find(param => param.name === parameter);
+        const parameterSettings = subcategorySettings.parameters.find(
+            (param) => param.name === parameter,
+        );
         if (!parameterSettings) {
             console.warn(`There is no parameter ${parameter} for OBS settings category ${category}.${subcategory}`);
             return [];
         }
 
-        return parameterSettings.values.map(value => Object.values(value)[0]);
+        return parameterSettings.values.map((value) => Object.values(value)[0]);
     }
 
     public resizePreview(bounds) {
@@ -307,10 +322,11 @@ export class OBS {
         osn.NodeObs.OBS_content_resizeDisplay("previewDisplay", displayWidth * scaleFactor, displayHeight * scaleFactor);
         osn.NodeObs.OBS_content_moveDisplay("previewDisplay", displayX * scaleFactor, displayY * scaleFactor);
 
-        return { height: displayHeight }
+        return { height: displayHeight };
     }
 
     private displayInfo() {
+        // eslint-disable-next-line
         const { screen } = require("electron");
         const primaryDisplay = screen.getPrimaryDisplay();
         const { width, height } = primaryDisplay.size;
@@ -318,11 +334,10 @@ export class OBS {
         return {
             width,
             height,
-            scaleFactor: scaleFactor,
+            scaleFactor,
             aspectRatio: width / height,
             physicalWidth: width * scaleFactor,
             physicalHeight: height * scaleFactor,
-        }
+        };
     }
-
 }
