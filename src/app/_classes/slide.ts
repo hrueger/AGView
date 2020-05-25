fe9bab8e262a3d5de507f1574a1d9718a3dac2cf
Hannes Rüger <hannesrueger@gmx.de>
2020-05-25 16:08:39 +0200

    added alignment options to slide class

--- src/app/_classes/slide.ts
+++ src/app/_classes/slide.ts
@@ -1,17 +1,29 @@
 import { Save } from "../_decorators/save.decorator";
+import { AlignmentOptions } from "./alignmentOptions";
 
 export class Slide {
-  @Save
-  public type: "browser" | "video" | "image";
+    @Save
+    public type: "browser" | "video" | "image";
 
-  @Save
-  public filePath: string;
+    @Save
+    public filePath: string;
 
-  @Save
-  public name: string;
+    @Save
+    public name: string;
 
-  @Save
-  public id: string;
+    @Save
+    public id: string;
 
-  public thumbnail?: string;
+    @Save
+    public alignment: AlignmentOptions;
+
+    public thumbnail?: string;
+
+    constructor() {
+        this.alignment = {
+            alignment: "center",
+            padding: 0,
+            scale: "fit",
+        };
+    }
 }
