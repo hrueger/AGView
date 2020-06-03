892a78e486834d4893243b761fde5b015f5a3092
Hannes Rüger <hannesrueger@gmx.de>
2020-06-03 15:48:00 +0200

    fixed obs not shut down

--- main.js
+++ main.js
@@ -117,12 +117,17 @@ function createWindow() {
     electron_1.ipcMain.on("settings-changed", function () {
         obs.updateSettings(win);
     });
+    electron_1.ipcMain.on("shutdown", function () {
+        obs.shutdown();
+    });
     // Emitted when the window is closed.
     win.on("closed", function () {
         // Dereference the window object, usually you would store window
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
-        win = null;
+        obs.shutdown();
+        console.log("shut down");
+        electron_1.app.quit();
     });
     return win;
 }
@@ -133,21 +138,6 @@ try {
     // Some APIs can only be used after this event occurs.
     // Added 400 ms to fix the black background issue while using transparent window. More detais at https://github.com/electron/electron/issues/15947
     electron_1.app.on("ready", function () { return setTimeout(createWindow, 400); });
-    // Quit when all windows are closed.
-    electron_1.app.on("window-all-closed", function () {
-        // On OS X it is common for applications and their menu bar
-        // to stay active until the user quits explicitly with Cmd + Q
-        if (process.platform !== "darwin") {
-            electron_1.app.quit();
-        }
-    });
-    electron_1.app.on("activate", function () {
-        // On OS X it"s common to re-create a window in the app when the
-        // dock icon is clicked and there are no other windows open.
-        if (win === null) {
-            createWindow();
-        }
-    });
 }
 catch (e) {
     // Catch Error
--- main.ts
+++ main.ts
@@ -120,13 +120,18 @@ function createWindow(): BrowserWindow {
     ipcMain.on("settings-changed", () => {
         obs.updateSettings(win);
     });
+    ipcMain.on("shutdown", () => {
+        obs.shutdown();
+    });
 
     // Emitted when the window is closed.
     win.on("closed", () => {
         // Dereference the window object, usually you would store window
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
-        win = null;
+        obs.shutdown();
+        console.log("shut down");
+        app.quit();
     });
 
     return win;
@@ -141,22 +146,6 @@ try {
     // Added 400 ms to fix the black background issue while using transparent window. More detais at https://github.com/electron/electron/issues/15947
     app.on("ready", () => setTimeout(createWindow, 400));
 
-    // Quit when all windows are closed.
-    app.on("window-all-closed", () => {
-        // On OS X it is common for applications and their menu bar
-        // to stay active until the user quits explicitly with Cmd + Q
-        if (process.platform !== "darwin") {
-            app.quit();
-        }
-    });
-
-    app.on("activate", () => {
-        // On OS X it"s common to re-create a window in the app when the
-        // dock icon is clicked and there are no other windows open.
-        if (win === null) {
-            createWindow();
-        }
-    });
 } catch (e) {
     // Catch Error
     // throw e;
