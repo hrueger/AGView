4ea9c6d55d8d7c2fb0dae6b26bde9a518e1c8c95
Hannes Rüger <hannesrueger@gmx.de>
2020-06-11 16:44:09 +0200

    fixed ffmpeg path

--- src/app/_services/thumbnail.service.ts
+++ src/app/_services/thumbnail.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from "@angular/core";
 import * as path from "path";
 import { remote } from "electron";
 import * as fs from "fs";
-import * as os from "os";
 import * as crypto from "crypto";
 import * as thumbnail from "simple-thumbnail";
 import { supportedFiles, SlideType } from "../_globals/supportedFilesFilters";
@@ -12,7 +11,7 @@ import { supportedFiles, SlideType } from "../_globals/supportedFilesFilters";
 })
 export class ThumbnailService {
     private thumbnailPath = path.join(remote.app.getPath("userData"), "thumbnails");
-    private ffmpegPath = path.join(__dirname, "../../../../../../bin", os.platform(), os.arch(), os.platform() == "win32" ? "ffmpeg.exe" : "ffmpeg");
+    private ffmpegPath = path.join(__dirname, "../../../../../../bin", "ffmpeg.exe").replace("app.asar", "");
 
     constructor() {
         if (!fs.existsSync(this.thumbnailPath)) {
